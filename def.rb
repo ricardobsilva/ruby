1b8e7a26310d32a969f9733b7dc29498fbd7d4a7
Ricardo <ricardo.b.silva13@gmail.com>
2016-07-03 10:56:56 -0300

    funções

---
+++ def.rb
@@ -0,0 +1,7 @@
+def name(my_name, last_name)
+  puts "meu primeiro nome é #{my_name} e meu sobrenome é #{last_name}"
+end
+
+name("Ricardo", "Silva")
+name("João", "Alguma coisa")
+name("Maria", "José")
